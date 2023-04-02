199cfe601b2318726aeb40b05d84a6712a329667
marktaddonio5 <marktaddonio5@gmail.com>
2023-04-02 16:59:34 +0200

    worked on form validation with formik and yup, need to continue

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,40 +11,85 @@ maybe will use useReducer to store?
 
 password1 and password2 must be the same 
 */
+import {useFormik} from "formik"
+import * as Yup from "yup";
+
+
 
 const Form = () => {
-    function handleSubmit(){
-
-    }
+    const formik = useFormik({
+        initialValues: {
+            fname:"",
+            lname:"",
+            uname:"",
+            email:"",
+            password: undefined,
+            phone: "",
+        },
+        validationSchema: Yup.object().shape({
+            fname: Yup.string()
+                .required("Please enter your first name"),
+            lname: Yup.string()
+                .required("Please enter your last name"),
+            uname: Yup.string()
+                .required("Please create a user name")
+                .min(3, "must be at least three characters long")
+                .max(20, "User name cannot be more than twenty characters long"),
+            email: Yup.string()
+                .email("Invalid email address")
+                .required("Please enter an email address"),
+            password: Yup.string()
+                .required("Please create a password"),
+            phone: Yup.string()
+                .max(12, "please enter a valid phone number")
+        })
+    })
 
     return (
-       
-        <form onSubmit={handleSubmit}>
+        <form onSubmit="">
             <h1>Create a new account!</h1>
             <label for="fname">First Name</label>
             <input
                 id="fname"
                 type="text"
                 placeholder="First name"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.fname}
+                ></input>
+            {formik.touched && formik.errors.fname ? (
+                <span classname="error">{formik.errors.fname}</span>
+            ) : null}
             <label for="lname">Last Name</label>
             <input
                 id="lname"
                 type="text"
                 placeholder="Last name"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.lname}
+                ></input>
             <label for="uname">Create User Name</label>
             <input
                 id="uname"
                 type="text"
                 placeholder="User name"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.uname}
+                ></input>
+            {formik.touched && formik.errors.uname ? (
+                <span classname="error">{formik.errors.uname}</span>
+            ) : null}
             <label for="email">Email Address</label>
             <input
                 id="email"
                 type="text"
                 placeholder="johnDoe@gmail.com"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                ></input>
             <label for="password1">Choose a password</label>
             <input
                 id="password1"
@@ -52,17 +97,25 @@ const Form = () => {
                 placeholder="(must be 5-14 characters)"
                 minLength="5"
                 maxLength="15"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                ></input>
             <label for="password2">Re-type password</label>
             <input
                 id="password2"
                 type="password"
-                required></input>
+                required
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                ></input>
             <label for="phone">Phone Number</label>
             <input
                 id="phone"
                 type="tel"
                 placeholder="123-456-7891"
+                onChange={formik.handleChange}
+                value={formik.values.phone}
                 ></input>
             <input className="formButton" type='submit' value="submit"></input>
         </form>
